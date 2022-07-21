c4145c105b4abd265febf39c1d2e6a69d5486090
Ben <benwoodyear@gmail.com>
2022-07-21 22:55:31 +0100

    Test 3 kernel sizes

--- code/quick_multi_run.sh
+++ code/quick_multi_run.sh
@@ -1,5 +1,9 @@
-python3 train.py --training_type multi-task_labels --label_level 1 --epochs 10 --seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 2 --epochs 10 --seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 3 --epochs 10 --seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 4 --epochs 10 --seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 5 --epochs 10 --seeds 1 -v
+#python3 train.py --training_type multi-task_labels --label_level 1 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task_labels --label_level 2 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task_labels --label_level 3 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task_labels --label_level 4 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task_labels --label_level 5 --epochs 10 --seed 0 -v
+
+python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 0 -v
+python3 train.py --training_type multi-task --kernel_size 5 --epochs 10 --seed 0 -v
+python3 train.py --training_type multi-task --kernel_size 7 --epochs 10 --seed 0 -v
