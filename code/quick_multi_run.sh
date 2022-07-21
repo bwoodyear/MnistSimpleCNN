82e6a35a8acd858195da12bf9ad78ebc1dcf857f
Ben <benwoodyear@gmail.com>
2022-07-21 22:18:24 +0100

    same issue again

--- code/quick_multi_run.sh
+++ code/quick_multi_run.sh
@@ -1,5 +1,5 @@
-python3 train.py --training_type multi-task_labels --label_level 1 --epochs 10 -seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 2 --epochs 10 -seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 3 --epochs 10 -seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 4 --epochs 10 -seeds 1 -v
-python3 train.py --training_type multi-task_labels --label_level 5 --epochs 10 -seeds 1 -v
+python3 train.py --training_type multi-task_labels --label_level 1 --epochs 10 --seeds 1 -v
+python3 train.py --training_type multi-task_labels --label_level 2 --epochs 10 --seeds 1 -v
+python3 train.py --training_type multi-task_labels --label_level 3 --epochs 10 --seeds 1 -v
+python3 train.py --training_type multi-task_labels --label_level 4 --epochs 10 --seeds 1 -v
+python3 train.py --training_type multi-task_labels --label_level 5 --epochs 10 --seeds 1 -v
