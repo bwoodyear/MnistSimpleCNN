5f1e363dd19c1c96d3f8b13671233ad83b97e8f0
Ben <benwoodyear@gmail.com>
2022-07-22 09:15:38 +0100

    kernel seed with another seed

--- code/quick_multi_run.sh
+++ code/quick_multi_run.sh
@@ -4,6 +4,10 @@
 #python3 train.py --training_type multi-task_labels --label_level 4 --epochs 10 --seed 0 -v
 #python3 train.py --training_type multi-task_labels --label_level 5 --epochs 10 --seed 0 -v
 
-python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 0 -v
-python3 train.py --training_type multi-task --kernel_size 5 --epochs 10 --seed 0 -v
-python3 train.py --training_type multi-task --kernel_size 7 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task --kernel_size 5 --epochs 10 --seed 0 -v
+#python3 train.py --training_type multi-task --kernel_size 7 --epochs 10 --seed 0 -v
+
+python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 1 -v
+python3 train.py --training_type multi-task --kernel_size 5 --epochs 10 --seed 1 -v
+python3 train.py --training_type multi-task --kernel_size 7 --epochs 10 --seed 1 -v
