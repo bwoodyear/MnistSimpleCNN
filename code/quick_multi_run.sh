531173050722da4adbca8456d3ed8789a1d20fd7
Ben <benwoodyear@gmail.com>
2022-07-26 09:28:49 +0100

    Three class l2 lests

--- code/quick_multi_run.sh
+++ code/quick_multi_run.sh
@@ -23,8 +23,7 @@
 #python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 0 --norm l2 --reg_lambda 1e-4 -v
 #python3 train.py --training_type multi-task --kernel_size 3 --epochs 10 --seed 0 --norm l2 --reg_lambda 1e-5 -v
 
-python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l1 --reg_lambda 0
-python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l1 --reg_lambda 1e-7
-python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l1 --reg_lambda 1e-6
-python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l1 --reg_lambda 1e-5
-python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l1 --reg_lambda 1e-4
+python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l2 --reg_lambda 0
+python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l2 --reg_lambda 1e-7
+python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l2 --reg_lambda 1e-6
+python3 train.py --training_type multi-task --epochs 10 --seed 0 -v --norm l2 --reg_lambda 1e-5
